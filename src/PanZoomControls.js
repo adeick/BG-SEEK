c714d54296d3a9229e65f98b4f47957c53ec1222
adeick <a8deick@gmail.com>
2025-05-17 13:32:57 -0500

    Updated Pan Navigation

---
+++ src/PanZoomControls.js
@@ -0,0 +1,174 @@
+// PanZoomControls.js
+
+import * as THREE from 'three';
+
+export class PanZoomControls {
+    constructor(camera, domElement) {
+        this.camera = camera;
+        this.domElement = domElement;
+
+        this.enabled = true;
+
+        // Pan state
+        this.isPanning = false;
+        this.startPan = new THREE.Vector2();
+        this.panSpeed = 1.0;
+        this.basePanSpeed = 5.0;
+        this.referenceZoom = 40; // around the default camera Y start position
+
+        this.panVelocity = new THREE.Vector2(0, 0);
+        this.dampingFactor = 0.9;
+
+        // Zoom state
+        this.zoomSpeed = 0.5;
+        this.minZoom = 2;
+        this.maxZoom = 50;
+        this.zoomVelocity = 0;
+        this.zoomDamping = 0.85;
+
+        // Touch
+        this._startDist = 0;
+        this._startZoom = camera.position.y;
+
+        // Bind event handlers
+        this._onMouseDown = this._onMouseDown.bind(this);
+        this._onMouseMove = this._onMouseMove.bind(this);
+        this._onMouseUp = this._onMouseUp.bind(this);
+        this._onMouseWheel = this._onMouseWheel.bind(this);
+
+        this._onTouchStart = this._onTouchStart.bind(this);
+        this._onTouchMove = this._onTouchMove.bind(this);
+        this._onTouchEnd = this._onTouchEnd.bind(this);
+
+        this._bindEventListeners();
+    }
+
+    _bindEventListeners() {
+        this.domElement.addEventListener('mousedown', this._onMouseDown);
+        window.addEventListener('mousemove', this._onMouseMove);
+        window.addEventListener('mouseup', this._onMouseUp);
+        this.domElement.addEventListener('wheel', this._onMouseWheel, { passive: false });
+
+        this.domElement.addEventListener('touchstart', this._onTouchStart, { passive: false });
+        this.domElement.addEventListener('touchmove', this._onTouchMove, { passive: false });
+        this.domElement.addEventListener('touchend', this._onTouchEnd);
+    }
+
+    _onMouseDown(event) {
+        if (!this.enabled) return;
+        this.isPanning = true;
+        this.startPan.set(event.clientX, event.clientY);
+        this.panVelocity.set(0, 0);
+    }
+
+    _onMouseMove(event) {
+        if (!this.enabled || !this.isPanning) return;
+
+        const zoomFactor = this.camera.position.y / this.referenceZoom;
+        const deltaX = (event.clientX - this.startPan.x) * this.basePanSpeed * zoomFactor * 0.01;
+        const deltaY = (event.clientY - this.startPan.y) * this.basePanSpeed * zoomFactor * 0.01;
+
+        this.panVelocity.set(-deltaX, -deltaY);
+
+        this.camera.position.x += this.panVelocity.x;
+        this.camera.position.z += this.panVelocity.y;
+
+        this.startPan.set(event.clientX, event.clientY);
+    }
+
+    _onMouseUp() {
+        this.isPanning = false;
+    }
+
+    _onMouseWheel(event) {
+        if (!this.enabled) return;
+        event.preventDefault();
+
+        const direction = event.deltaY > 0 ? 1 : -1;
+        this.zoomVelocity += direction * this.zoomSpeed * 0.5;
+    }
+
+    _onTouchStart(event) {
+        if (event.touches.length === 1) {
+            this.isPanning = true;
+            this.startPan.set(event.touches[0].clientX, event.touches[0].clientY);
+            this.panVelocity.set(0, 0);
+        }
+
+        if (event.touches.length === 2) {
+            this._startDist = this._touchDistance(event);
+            this._startZoom = this.camera.position.y;
+        }
+    }
+
+    _onTouchMove(event) {
+        event.preventDefault();
+
+        if (event.touches.length === 1 && this.isPanning) {
+            const zoomFactor = this.camera.position.y / this.referenceZoom;
+            const deltaX = (event.touches[0].clientX - this.startPan.x) * this.basePanSpeed * zoomFactor * 0.01;
+            const deltaY = (event.touches[0].clientY - this.startPan.y) * this.basePanSpeed * zoomFactor * 0.01;
+
+            this.panVelocity.set(-deltaX, -deltaY);
+
+            this.camera.position.x += this.panVelocity.x;
+            this.camera.position.z += this.panVelocity.y;
+
+            this.startPan.set(event.touches[0].clientX, event.touches[0].clientY);
+        }
+
+        if (event.touches.length === 2) {
+            const newDist = this._touchDistance(event);
+            const zoomDelta = (this._startDist - newDist) * 0.1;
+
+            this.zoomVelocity += zoomDelta * this.zoomSpeed * 0.1;
+        }
+    }
+
+    _onTouchEnd() {
+        this.isPanning = false;
+    }
+
+    _touchDistance(event) {
+        const dx = event.touches[0].clientX - event.touches[1].clientX;
+        const dy = event.touches[0].clientY - event.touches[1].clientY;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    update() {
+        if (!this.enabled) return;
+
+        // Apply zoom damping
+        if (Math.abs(this.zoomVelocity) > 0.001) {
+            this.camera.position.y = THREE.MathUtils.clamp(
+                this.camera.position.y + this.zoomVelocity,
+                this.minZoom,
+                this.maxZoom
+            );
+            this.zoomVelocity *= this.zoomDamping;
+        } else {
+            this.zoomVelocity = 0;
+        }
+
+        // Apply pan inertia
+        if (!this.isPanning && (Math.abs(this.panVelocity.x) > 0.001 || Math.abs(this.panVelocity.y) > 0.001)) {
+            this.camera.position.x += this.panVelocity.x;
+            this.camera.position.z += this.panVelocity.y;
+
+            this.panVelocity.multiplyScalar(this.dampingFactor);
+        } else if (!this.isPanning) {
+            this.panVelocity.set(0, 0);
+        }
+    }
+
+    dispose() {
+        this.domElement.removeEventListener('mousedown', this._onMouseDown);
+        window.removeEventListener('mousemove', this._onMouseMove);
+        window.removeEventListener('mouseup', this._onMouseUp);
+        this.domElement.removeEventListener('wheel', this._onMouseWheel);
+
+        this.domElement.removeEventListener('touchstart', this._onTouchStart);
+        this.domElement.removeEventListener('touchmove', this._onTouchMove);
+        this.domElement.removeEventListener('touchend', this._onTouchEnd);
+    }
+}
--- src/script.js
+++ src/script.js
@@ -1,16 +1,11 @@
+/**
+ * Import Statements
+ */
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import { PanZoomControls } from './PanZoomControls';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import USA_Map_URL from '../static/BaseUSA.glb'
-import GUI from 'lil-gui'
-
-/**
- * Base
- */
-// Debug
-// const gui = new GUI()
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -20,61 +15,8 @@ const scene = new THREE.Scene()
 scene.background = new THREE.Color().setHex("0xc8cecf");
 
 /**
- * Textures
+ * Load USA Map 
  */
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('textures/matcaps/8.png')
-matcapTexture.colorSpace = THREE.SRGBColorSpace
-
-const material = new THREE.MeshStandardMaterial()
-material.roughness = 0.4
-
-// const plane = new THREE.Mesh(
-//     new THREE.SphereGeometry(30, 64, 32),
-//     material
-// )
-// // plane.rotation.x = - Math.PI * 0.5
-// plane.position.y = - 30
-// scene.add(plane)
-
-/**
- * Fonts
- */
-const fontLoader = new FontLoader()
-
-//Load "SEEK 2025" and //donuts
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font) => {
-        // Material
-        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-
-        // Text
-        // const textGeometry = new TextGeometry(
-        //     'SEEK 2025',
-        //     {
-        //         font: font,
-        //         size: 0.5,
-        //         depth: 0.2,
-        //         curveSegments: 12,
-        //         bevelEnabled: true,
-        //         bevelThickness: 0.03,
-        //         bevelSize: 0.02,
-        //         bevelOffset: 0,
-        //         bevelSegments: 5
-        //     }
-        // )
-        // textGeometry.center()
-        // 
-        // const text = new THREE.Mesh(textGeometry, material)
-        // text.position.z += -3;
-        // text.position.y += 1;
-        // text.rotateX(-Math.PI / 4)
-        // scene.add(text)
-    }
-)
-
-//Load USA Map
 const loader = new GLTFLoader();
 loader.load(USA_Map_URL, function (gltf) {
 
@@ -83,130 +25,17 @@ loader.load(USA_Map_URL, function (gltf) {
 }, undefined, function (error) {
 
     console.error(error);
-
 });
 
-const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2(-999, 999); //stop autoselecting kansas when starting
-
-let hoveredState = null;
-let hoveredStateId = -1; // faster comparison (don't really know if faster but I assume it would be)
-
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
 const normalMaterial = new THREE.MeshStandardMaterial();
 const hoverMaterial = new THREE.MeshStandardMaterial();
 
-function onPointerMove(event) {
-
-    // calculate pointer position in normalized device coordinates
-    // (-1 to +1) for both components
-
-    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-    pointer.y = - ((event.clientY - 60) / (window.innerHeight - 60)) * 2 + 1;
-
-    // console.log(event.clientY)
-}
-
-function raycastRender() {
-
-    // update the picking ray with the camera and pointer position
-    raycaster.setFromCamera(pointer, camera);
-
-    // calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects(scene.children);
-
-    let stateBeingHovered = false;
-    for (let i = 0; i < intersects.length; i++) {
-
-        if (hoveredState == null || hoveredStateId !== intersects[i].object.id) {
-            if (hoveredState != null) {
-                hoveredState.position.add(new THREE.Vector3(0, -0.1, 0));
-                hoveredState.material = normalMaterial
-            }
-            else {
-                //first state to be hovered
-                // console.log(intersects[i].object.name)
-                normalMaterial.copy(intersects[i].object.material);
-                hoverMaterial.copy(intersects[i].object.material);
-                hoverMaterial.color.set(0xcfe0fa)
-            }
-            // console.log(hoveredStateId + "  " + intersects[i].object.material.color.getHex()); //.material.color.set(0xff0000);
-            hoveredState = intersects[i].object;
-            // console.log(hoveredState.position)
-
-            hoveredState.position.add(new THREE.Vector3(0, 0.1, 0));
-            intersects[i].object.material = hoverMaterial
-
-            // hoveredState.material.color.set(0x000000)
-
-            // console.log(hoveredState.position)
-
-            hoveredStateId = intersects[i].object.id;
-            stateBeingHovered = true;
-            // console.log("New Hover on " + intersects[i].object.name); //.material.color.set(0xff0000);
-            return;
-        }
-        if (hoveredStateId == intersects[i].object.id) {
-            stateBeingHovered = true;
-            return;
-        }
-
-        // console.log(intersects[i].object.id); //.material.color.set(0xff0000);
-    }
-    if (!stateBeingHovered) {
-        if (hoveredState != null) {
-            hoveredState.position.add(new THREE.Vector3(0, -0.1, 0));
-            hoveredState.material = normalMaterial
-            hoveredState = null;
-        }
-        hoveredStateId = -1;
-        // console.log("No hover"); //.material.color.set(0xff0000);
-    }
-
-    renderer.render(scene, camera);
-
-}
-
-window.addEventListener('pointermove', onPointerMove);
 
 // Hemisphere light
 const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 4)
-// const hemisphereLight = new THREE.HemisphereLight(0xbfd8ff, 0x000000, 4)
 scene.add(hemisphereLight)
-
-// Point light
-// const pointLight = new THREE.PointLight(0xff9000, 1.5, 0, 2)
-// pointLight.position.set(1, - 0.5, 1)
-// scene.add(pointLight)
-
-// React area light
-// const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 6, 1, 1)
-// rectAreaLight.position.set(- 1.5, 0, 1.5)
-// rectAreaLight.lookAt(new THREE.Vector3())
-// scene.add(rectAreaLight)
-
-// Spot light
-// const spotLight = new THREE.SpotLight(0xffffff, 6, 45, Math.PI * 0.01, 1, 1)
-// spotLight.position.set(-2.5, 12, 0)
-// spotLight.target.position.x = -2.5
-// spotLight.target.position.z = -0.8
-// scene.add(spotLight)
-// scene.add(spotLight.target)
-
-// Helpers
-// const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
-// scene.add(hemisphereLightHelper)
-
-// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
-// scene.add(directionalLightHelper)
-
-// const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
-// scene.add(pointLightHelper)
-
-// const spotLightHelper = new THREE.SpotLightHelper(spotLight)
-// scene.add(spotLightHelper)
-
-// const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
-// scene.add(rectAreaLightHelper)
 
 /**
  * Sizes
@@ -231,25 +60,6 @@ window.addEventListener('resize', () => {
 })
 
 /**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 8
-camera.position.z = 4
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-controls.maxPolarAngle = Math.PI / 2 - Math.PI / 32
-controls.minPolarAngle = 0
-// controls.maxAzimuthAngle = Math.PI
-// controls.minAzimuthAngle = -1* Math.PI
-
-/**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
@@ -259,6 +69,150 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 2000)
+
+camera.position.set(0, 10, 0);
+camera.lookAt(0, 0, 0);
+
+scene.add(camera)
+
+// Controls
+const controls = new PanZoomControls(camera, canvas)
+controls.enableDamping = true
+controls.autoRotate = false
+controls.zoomToCursor = true;
+// controls.enableRotate = false
+// controls.maxPolarAngle = Math.PI / 4
+// controls.minPolarAngle = Math.PI / 4
+// controls.minAzimuthAngle = 0
+// controls.maxAzimuthAngle = 0
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2(-999, 999); //stop autoselecting kansas when starting
+
+let selectionSituation = 0; // 0 is null, 1 is hovering, 2 is selected
+let relevantState = null;
+let relevantStateId = null;
+
+function onPointerMove(event) {
+
+    // calculate pointer position in normalized device coordinates
+    // (-1 to +1) for both components
+
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - ((event.clientY - 60) / (window.innerHeight - 60)) * 2 + 1;
+
+    // console.log(event.clientY)
+}
+
+function onPointerDown(event) {
+    if (selectionSituation == 0) {
+        return; // Cannot select unhovered State (? maybe unnecessary on phone)
+    }
+
+    //Code below should only really execute in selectionSituation == 1 (so maybe add blocking code for selectionSituation == 2)
+
+    let gotoCameraPosition = new THREE.Vector3();
+    let validState = false;
+
+    switch (relevantStateId) {
+        case 31:
+            //Ohio
+            console.log("Ohio clicked " + camera)
+            validState = true;
+            gotoCameraPosition.set(2.414840103769524, 0.7005265324972118, -0.36776443756869115);
+        // camera.rotation.set(-Math.PI / 2, 0, 0);
+        // camera.lookAt(hoveredState.position)
+    }
+
+    if (!validState) return;
+
+    gsap.to(camera.position, {
+        duration: 1,
+        x: gotoCameraPosition.x,
+        y: gotoCameraPosition.y,
+        z: gotoCameraPosition.z,
+        onUpdate: function () {
+
+            controls.update();
+
+        },
+        onComplete: function () {
+
+            // controls.enabled = true;
+            // controls.target = unitedStatesTarget
+        }
+    });
+}
+
+function calculateStateHover(intersects) {
+    if (selectionSituation == 2)
+        return; //If a state is already selected, we aren't hovering
+
+
+    for (let i = 0; i < intersects.length; i++) {
+        if (intersects[i].object.id == relevantStateId) {
+            //Same State selected
+            return;
+        }
+
+        if (selectionSituation == 0) {
+            //Definitely a more efficient way to do this
+            normalMaterial.copy(intersects[i].object.material);
+            hoverMaterial.copy(intersects[i].object.material);
+            hoverMaterial.color.set(0xcfe0fa)
+        }
+        if (selectionSituation == 1) {
+            //Unselect previous relevantState
+            relevantState.position.add(new THREE.Vector3(0, -0.1, 0));
+            relevantState.material = normalMaterial
+        }
+
+        relevantState = intersects[i].object;
+        relevantState.position.add(new THREE.Vector3(0, 0.1, 0));
+        relevantState.material = hoverMaterial
+        relevantStateId = intersects[i].object.id;
+        // isAStateBeingHovered = true;
+        selectionSituation = 1;
+        return;
+    }
+
+    if (selectionSituation == 1) {
+        //Unselect previous relevantState
+        relevantState.position.add(new THREE.Vector3(0, -0.1, 0));
+        relevantState.material = normalMaterial
+        relevantState = null;
+    }
+
+    selectionSituation = 0;
+    relevantStateId = -1
+
+    return;
+
+}
+
+
+function raycastRender() {
+    // update the picking ray with the camera and pointer position
+    raycaster.setFromCamera(pointer, camera);
+
+    // calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects(scene.children);
+
+    calculateStateHover(intersects);
+    renderer.render(scene, camera);
+}
+
+
+
+window.addEventListener('pointermove', onPointerMove);
+window.addEventListener('pointerdown', onPointerDown);
+
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
@@ -266,15 +220,77 @@ const clock = new THREE.Clock()
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
+    // if (selectionSituation < 2) {
     // Update controls
+    // console.log("updateControls")
     controls.update()
-
+    // }
     // Render
     renderer.render(scene, camera)
     raycastRender()
-
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
 tick()
+
+
+// Old Algorithms
+
+function onPointerDownX(event) {
+    // calculate pointer position in normalized device coordinates
+    // (-1 to +1) for both components
+
+
+    // if (selectionSituation == 0) {
+    //     return; // Cannot select unhovered State (? maybe unnecessary on phone)
+    // }
+    if (selectionSituation == 2) {
+        selectionSituation = 0;
+        console.log(" pos " + camera.position.x + " " + camera.position.y + " " + camera.position.z)
+
+        return; // Toggle
+    }
+
+    //Unselect previous relevantState
+    relevantState.position.add(new THREE.Vector3(0, -0.1, 0));
+    relevantState.material = normalMaterial
+    selectionSituation = 2;
+
+
+    // controls.enabled = false;
+    // camera.rotation.set(-Math.PI / 2, 0, 0);
+    // let gotoCameraPosition = new THREE.Vector3();
+    // let validState = false;
+
+    // switch (relevantStateId) {
+    //     case 31:
+    //         //Ohio
+    //         validState = true;
+    //         gotoCameraPosition.set(2.414840103769524, 0.7005265324972118, -0.36776443756869115);
+    //     // camera.rotation.set(-Math.PI / 2, 0, 0);
+    //     // camera.lookAt(hoveredState.position)
+    // }
+
+    // if (!validState) return;
+
+    // controls.enabled = false;
+    // controls.target = relevantState.position
+    // gsap.to(camera.position, {
+    //     duration: 1,
+    //     x: gotoCameraPosition.x,
+    //     y: gotoCameraPosition.y,
+    //     z: gotoCameraPosition.z,
+    //     onUpdate: function () {
+
+    //         controls.update();
+
+    //     },
+    //     onComplete: function () {
+
+    //         controls.enabled = true;
+
+    //     }
+    // });
+
+}
